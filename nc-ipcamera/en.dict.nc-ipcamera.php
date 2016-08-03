64a75dad516d66d3a42d89db0607d12e84ebeb32
cozan78 <cozan@santafe.gov.ar>
2016-08-03 14:37:09 -0300

    New EC IP Camera

---
+++ nc-ipcamera/en.dict.nc-ipcamera.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2015 Federico Lazcano
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+	'Class:IPCamera' => 'IP Camera',
+	'Class:IPCamera+' => 'An IP Security Camera',
+	'Class:IPCamera/Attribute:type' => 'Type',
+	'Class:IPCamera/Attribute:type+' => 'Camera type',
+	'Class:IPCamera/Attribute:type/Value:ptz' => 'PTZ Dome',
+	'Class:IPCamera/Attribute:type/Value:fixed' => 'Fixed',
+	'Class:IPCamera/Attribute:case' => 'Case',
+	'Class:IPCamera/Attribute:case+' => 'Case type',
+	'Class:IPCamera/Attribute:case/Value:indoor' => 'Indoor',
+	'Class:IPCamera/Attribute:case/Value:outodoor' => 'Outdoor',
+));
+?>
---
+++ nc-ipcamera/es_cr.dict.nc-ipcamera.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2015 Federico Lazcano
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('ES CR', 'Spanish', 'Español, Castellano', array(
+	'Class:IPCamera' => 'Cámara IP',
+	'Class:IPCamera+' => 'Cámara de Seguridad IP',
+	'Class:IPCamera/Attribute:type' => 'Tipo',
+	'Class:IPCamera/Attribute:type+' => 'Tipo de cámara',
+	'Class:IPCamera/Attribute:type/Value:ptz' => 'Domo móvil',
+	'Class:IPCamera/Attribute:type/Value:fixed' => 'Fija',
+	'Class:IPCamera/Attribute:case' => 'Carcasa',
+	'Class:IPCamera/Attribute:case+' => 'Tipo de carcasa',
+	'Class:IPCamera/Attribute:case/Value:indoor' => 'Interior',
+	'Class:IPCamera/Attribute:case/Value:outdoor' => 'Exterior',
+));
+?>
